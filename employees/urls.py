de4de128a11b3da89ba64b03884384a157fb951c
Anuranj-48 <anuranjkrishnakadankodan@gmail.com>
2026-05-10 16:35:40 +0530

    Updated Employee Management System

--- employees/urls.py
+++ employees/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, add_employee, delete_employee, edit_employee
 
 urlpatterns = [
     path('',home, name='home'),
+    path('add/', add_employee, name='add_employee'),
+    path('delete/<int:id>/', delete_employee, name='delete_employee'),
+    path('edit/<int:id>/', edit_employee, name='edit_employee'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Employee
+from .forms import EmployeeForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def home(request):
-    return render(request, 'home.html')
 
+    search = request.GET.get('search')
+
+    if search:
+        employees = Employee.objects.filter(name__icontains=search)
+    else:
+        employees = Employee.objects.all()
+
+    return render(request, 'home.html', {
+        'employees': employees
+    })
+
+
+@login_required
+def add_employee(request):
+
+    form = EmployeeForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+
+    return render(request, 'add_employee.html', {
+        'form': form
+    })
+
+
+@login_required
+def delete_employee(request, id):
+
+    employee = Employee.objects.get(id=id)
+
+    employee.delete()
+
+    return redirect('/')
+
+
+@login_required
+def edit_employee(request, id):
+
+    employee = Employee.objects.get(id=id)
+
+    form = EmployeeForm(request.POST or None, instance=employee)
+
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+
+    return render(request, 'add_employee.html', {
+        'form': form
+    })
